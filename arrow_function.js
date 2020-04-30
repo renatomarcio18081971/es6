ecb1bd461698a52fa5edbb3f7b0a32ae8b4cd283
renatomarcio18081971 <renato.marcio@gmail.com>
2020-04-30 18:36:53 -0300

    finalizado exemplo

---
+++ arrow_function.js
@@ -0,0 +1,14 @@
+var dobroDoValor = numero => {
+    return numero * 2;
+}
+
+var dobroDoValor2 = (numero, numero1) => {
+    return (numero + numero1) / 10
+}
+
+var dobroDoValor3 = numero => numero * 2;
+
+
+console.log(dobroDoValor(7));
+console.log(dobroDoValor2(5,5));
+console.log(dobroDoValor3(10))
